8ad7dc7d3c3395f8a59af9adf66e775f964b532d
paulina jarmula <pjarmula@gmail.com>
2019-06-06 20:54:24 +0200

    changes in html new functions file

---
+++ js/functions.js
@@ -0,0 +1,8 @@
+function printMessage(msg){
+var div = document.createElement('div');
+div.innerHTML = msg;
+document.getElementById('messages').appendChild(div);
+}
+function clearMessages(){
+document.getElementById('messages').innerHTML = '';
+}
